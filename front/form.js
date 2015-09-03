81f35d92e08a9c131642d59b91dfdf9c7b047217
Yaroslav Andryushchenkov <7098013@gmail.com>
2015-09-04 00:11:06 +0300

    Adding new items implemented

--- front/form.js
+++ front/form.js
@@ -255,6 +255,33 @@ staffApp.controller('itemsCtrl', function ($scope, $http) {
         $scope.items = data;
     });
 
+    $scope.newItem = {};
+
+    function emptyNewItem() {
+        $scope.newItem.name = '';
+        $scope.newItem.email = '';
+        $scope.newItem.phone = '';
+    }
+
+    emptyNewItem();
+
+    $scope.addNewItem = function(item){
+        var postData = "";
+        var conjunction = "";
+
+        for(field in item) {
+            postData += conjunction + field + '=' + encodeURIComponent(item[field]);
+            if (conjunction === '')
+                conjunction = "&";
+
+        }
+
+        $http.post('/items', postData).success(function(data){
+            $scope.items.push(data);
+            emptyNewItem();
+        })
+    }
+
     $scope.deleteItem = function(id) {
         $http.delete('/items?id='+id).success(function(data){
             for (index in $scope.items) {
