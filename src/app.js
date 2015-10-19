5cee1350d1100f98c8eaf59931570d1ee6db7285
Alisa Isterina <alisa_isterina@epam.com>
2015-10-19 23:15:05 +0300

    Add logic to view Items on the same page in a table

--- src/app.js
+++ src/app.js
@@ -1,16 +1,46 @@
 (function () {
     var itemApp,
-        ItemController;
+        ItemController,
+        ItemFromResponseService;
 
-    ItemController = function($http) {
-        this.create = function(user){
-            $http.post('/items', user).success(function(data) {
-                console.log('Posted', data);
+    ItemController = function($http, ItemFromResponseService) {
+        var self = this;
+
+        this.items = [];
+
+        this.create = function(item){
+            $http.post('/items', item).success(function(data) {
+                self.items.push(ItemFromResponseService.get(data));
             });
+        };
+
+        this.get = function() {
+            $http.get('/items').success(function(data) {
+                angular.forEach(data, function(obj){
+                    self.items.push(ItemFromResponseService.get(obj));
+                });
+            });
+        };
+        this.get();
+    };
+
+    ItemFromResponseService = function () {
+        this.get = function (responseObj) {
+            var objId;
+            var obj = {};
+            angular.forEach(responseObj, function (v, k) {
+                if (v == '') {
+                    obj = JSON.parse(k);
+                } else if (k == 'id') {
+                    objId = v;
+                }
+            });
+            obj.id = objId;
+            return obj;
         };
     };
 
     itemApp = angular.module('itemApp', [])
-        .controller('itemController', ['$http', ItemController]);
-
+        .controller('itemController', ['$http', 'ItemFromResponseService', ItemController])
+        .service('ItemFromResponseService', ItemFromResponseService);
 })();
